7a2900482807ddba33fe8668b09c80e870312759
Shekil Rahman <154861734+shekilrahman@users.noreply.github.com>
2024-06-12 02:43:34 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
-export const PORT = 5555;
+export const PORT = 3000;
 export const mongoDBURL = 'mongodb+srv://root:root@bookstore.ffqtb0m.mongodb.net/books-collection?retryWrites=true&w=majority&appName=bookstore';
